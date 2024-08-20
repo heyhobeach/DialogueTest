c6cf5194a4ac92c20daad0e60993ffcaaf3edf8d
heyhobeach <zxc120021@gmail.com>
2024-08-20 20:05:29 +0900

    .

--- Assets/DatabaseManager.cs
+++ Assets/DatabaseManager.cs
@@ -38,7 +38,6 @@ public class DatabaseManager : MonoBehaviour
             csv_FileName= string.Format("{0}\\{1}\\Chapter{2}\\Chapter{3}", "FindTest", CheckLangugea(eLang), chapter,chapter);
             //Dialogue[] dialogues =theParser.Parse(csv_FileName);//여기서 지금 대화 모든 내용을 다 파싱 한 상태//주석 풀고 사용 하면 됨
             Dialogue[] dialogues =theParser.Parse(csv_FileName);//여기서 지금 대화 모든 내용을 다 파싱 한 상태//주석 풀고 사용 하면 됨
-            
             for (int i =0;i<dialogues.Length;i++)
             {
                 dialogueDic.Add(i +1, dialogues[i]);
